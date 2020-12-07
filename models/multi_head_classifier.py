891dc36b372dd18c56a083266b7fda4f7f39265f
growsafe\rahim.kassanaly <rahim.kassanaly@growsafe.com>
2020-12-07 11:42:26 -0700

    change multi-head with different projection per output

--- models/multi_head_classifier.py
+++ models/multi_head_classifier.py
@@ -6,7 +6,25 @@ class Multi_head(nn.Module):
     def __init__(self, num_classes_per_task=2):
         super(Multi_head, self).__init__()
 
-        self.projection = nn.Sequential(
+        self.projection1 = self.build_projection()
+
+        self.projection2 = self.build_projection()
+
+        self.projection3 = self.build_projection()
+
+        self.projection4 = self.build_projection()
+
+        self.projection5 = self.build_projection()
+
+        self.out1 = nn.Linear(4096, num_classes_per_task)
+        self.out2 = nn.Linear(4096, num_classes_per_task)
+        self.out3 = nn.Linear(4096, num_classes_per_task)
+        self.out4 = nn.Linear(4096, num_classes_per_task)
+        self.out5 = nn.Linear(4096, num_classes_per_task)
+
+    def build_projection(self):
+
+        return nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 2 * 2, 4096),
             nn.ReLU(inplace=True),
@@ -15,29 +33,28 @@ class Multi_head(nn.Module):
             nn.ReLU(inplace=True),
         )
 
-        self.out1 = nn.Linear(4096, num_classes_per_task)
-        self.out2 = nn.Linear(4096, num_classes_per_task)
-        self.out3 = nn.Linear(4096, num_classes_per_task)
-        self.out4 = nn.Linear(4096, num_classes_per_task)
-        self.out5 = nn.Linear(4096, num_classes_per_task)
-
     def forward(self, x, task_id=0):
 
-        x  =self.projection(x)
+
 
         if task_id == 0:
+            x = self.projection1(x)
             x1 = self.out1(x)
             return x1
         elif task_id == 1:
+            x = self.projection2(x)
             x2 = self.out2(x)
             return x2
         elif task_id == 2:
+            x = self.projection3(x)
             x3 = self.out3(x)
             return x3
         elif task_id == 3:
+            x = self.projection4(x)
             x4 = self.out4(x)
             return x4
         elif task_id == 4:
+            x = self.projection5(x)
             x5 = self.out5(x)
             return x5
 
